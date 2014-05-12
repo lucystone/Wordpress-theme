cb0aa4144e59e4af49d337371af37b6cde458947
Lucy Stone <lucy.stone@Bootcamps-iMac-6.local>
2014-05-12 16:14:38 +1200

    added post previous buttons,centered images, added contact page

--- index.php
+++ index.php
@@ -8,7 +8,8 @@
    		<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 		 <?php if ( has_post_thumbnail() ) { ?>
          <div class="gridly-image"><a href="<?php the_permalink() ?>"><?php the_post_thumbnail( 'summary-image' );  ?></a></div>
-          
+
+
        
 		  <?php } ?>
        			<div class="gridly-copy"><h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
